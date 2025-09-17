f57da19c58c6834dff80b3cc5fd67bf95a0e2e77
Joseph Jäger Bustillos <121409450+JosephJB2711@users.noreply.github.com>
2025-09-17 22:48:23 +0200

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ import pandas as pd
 import numpy as np
 import yfinance as yf
 import plotly.graph_objects as go
+import plotly.express as px
+from plotly.subplots import make_subplots
 import datetime as dt
 # -----------------------------------------------------------------------------
 # Grundkonfiguration
